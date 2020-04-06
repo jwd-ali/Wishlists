177a12cd9acd962bbd9d5423eeb162d82b45ae1c
Chris <chriskonnerth@gmx.de>
2020-04-06 19:06:31 +0200

    WishView: UIView fixed

--- Wishlist/View Controllers/HomeVC/MakeWishView/WishWishView.swift
+++ Wishlist/View Controllers/HomeVC/MakeWishView/WishWishView.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class WishView: UIStackView, UITextFieldDelegate { 
+class WishView: UIView, UITextFieldDelegate { 
     
     public var height = CGFloat(0)
     
